3212c13aff3c58b06139f0bc93f1663a35adc071
Cristi Mihai <cristi.mihai@gmail.com>
2017-07-29 23:10:19 +0100

    layering, collision detection

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	private Animator anim;
+	public Animator anim;
+//	private CharacterController characterController;
 	
 	void Start ()
 	{
-		anim = GetComponent<Animator>();
+		anim = GetComponentInChildren<Animator>();
+//		characterController = GetComponent<CharacterController>();
 	}
 	
 	void Update ()
@@ -17,8 +19,13 @@ public class PlayerController : MonoBehaviour
 		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
 
 		transform.localScale = new Vector3(move.x > 0 ? -1 : 1, 1, 1);
+
+		var rb = GetComponent<Rigidbody2D>();
+		rb.velocity = move * maxSpeed * Time.deltaTime * 50f;
 		
-		transform.position += move * maxSpeed * Time.deltaTime;
+		
+//		 transform.position += move * maxSpeed * Time.deltaTime;
+//		characterController.Move(move * maxSpeed * Time.deltaTime);
 
 		anim.SetFloat("speed", Math.Abs(move.x));
 
---
+++ Assets/TileMapTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileMapTest : MonoBehaviour
+{
+	public GameObject player;	
+	public Tilemap map;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		map = GetComponent<Tilemap>();
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+		 var tilePos = map.WorldToCell(player.transform.position);
+
+		var tran = map.GetTransformMatrix(tilePos);
+		tran.m33 = -1f;
+		map.SetTransformMatrix(tilePos, tran);
+	}
+}
