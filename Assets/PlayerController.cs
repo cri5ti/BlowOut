ca2ca92114c30f177bd0b98d60aff2e7e056f103
Cristi Mihai <cristi.mihai@gmail.com>
2017-07-29 16:19:18 +0100

    basic walk

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update ()
+	{
+		var speed = 2.0f;
+
+		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+		transform.localScale = new Vector3(move.x > 0 ? -1 : 1, 1, 1);
+		
+		transform.position += move * speed * Time.deltaTime;
+		
+//		var pos = transform.position;
+//		pos.x += move;
+//		transform.position = pos;
+		//var rb = GetComponent<Rigidbody2D>();
+		// rb.velocity = new Vector2(move * 5f, rb.velocity.y);
+	}
+}
