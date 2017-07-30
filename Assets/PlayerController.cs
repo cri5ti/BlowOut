8919b0c4a17552e6ed2541c507fd8cd6c6d0aec4
Cristi Mihai <cristi.mihai@gmail.com>
2017-07-30 14:15:02 +0100

    more tiles, walk animator fix

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,30 +5,33 @@ public class PlayerController : MonoBehaviour
 {
 	public Animator anim;
 //	private CharacterController characterController;
+	private Rigidbody2D rigidBody;
 	
 	void Start ()
 	{
 		anim = GetComponentInChildren<Animator>();
+		rigidBody = GetComponent<Rigidbody2D>();
+	
 //		characterController = GetComponent<CharacterController>();
 	}
 	
 	void Update ()
 	{
-		var maxSpeed = 3.0f;
+		var maxSpeed = 6.0f;
 
-		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		var input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
 
-		transform.localScale = new Vector3(move.x > 0 ? -1 : 1, 1, 1);
-
-		var rb = GetComponent<Rigidbody2D>();
-		rb.velocity = move * maxSpeed * Time.deltaTime * 50f;
+		input.Normalize();
 		
+		// flip axis
+		transform.localScale = new Vector3(input.x > 0 ? -1 : 1, 1, 1);
+
+		var velocity = input * maxSpeed * Time.deltaTime * 50f;
+
+		rigidBody.velocity = velocity;
 		
-//		 transform.position += move * maxSpeed * Time.deltaTime;
-//		characterController.Move(move * maxSpeed * Time.deltaTime);
-
-		anim.SetFloat("speed", Math.Abs(move.x));
-
-		anim.SetFloat("upDown", move.y);
+		anim.SetFloat("leftRight", Math.Abs(velocity.x));
+		anim.SetFloat("up", Math.Max(velocity.y, 0));
+		anim.SetFloat("down", Math.Max(-velocity.y, 0));
 	}
 }
